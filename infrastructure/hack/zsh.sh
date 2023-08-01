924892a4bb46d297f0d352562f87c071e77d5d14
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-08-01 16:09:53 +0000

    retrieved back changes to zsh as it was resulting in flaky errors in installation

--- infrastructure/hack/zsh.sh
+++ infrastructure/hack/zsh.sh
@@ -6,7 +6,7 @@ install_zsh() {
     sudo apt install -y zsh
 
     # Install Oh My Zsh
-    echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     # Install Autosuggestions plugin for Oh My Zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -15,7 +15,7 @@ install_zsh() {
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
 
     # Set Zsh as the default shell
-    echo "Y" | chsh -s "$(which zsh)"
+    chsh -s "$(which zsh)"
 
     # Restart the shell
     exec zsh
