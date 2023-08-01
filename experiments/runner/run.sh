e7db36f79a1680a403cfed7b0cb9c2f540bee31e
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-08-01 19:44:13 +0000

    extra files removed

---
+++ experiments/runner/run.sh
@@ -0,0 +1,42 @@
+# TODO add activation of the environment
+
+python runner_script.py --config-name video-1
+sleep 60
+python runner_script.py --config-name video-2
+sleep 60
+python runner_script.py --config-name video-3
+sleep 60
+python runner_script.py --config-name video-4
+sleep 60
+python runner_script.py --config-name video-5
+sleep 60
+python runner_script.py --config-name video-6
+sleep 60
+python runner_script.py --config-name video-7
+sleep 60
+python runner_script.py --config-name video-8
+sleep 60
+python runner_script.py --config-name video-9
+sleep 60
+python runner_script.py --config-name video-10
+sleep 60
+python runner_script.py --config-name video-11
+sleep 60
+python runner_script.py --config-name video-12
+sleep 60
+python runner_script.py --config-name video-13
+sleep 60
+python runner_script.py --config-name video-14
+sleep 60
+python runner_script.py --config-name video-15
+sleep 60
+python runner_script.py --config-name video-16
+sleep 60
+python runner_script.py --config-name video-17
+sleep 60
+python runner_script.py --config-name video-18
+sleep 60
+python runner_script.py --config-name video-19
+sleep 60
+python runner_script.py --config-name video-20
+sleep 60
