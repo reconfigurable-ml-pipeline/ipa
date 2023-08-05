9b69060f8060fbe44bfa9326331d7dcf0003bd07
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-08-05 22:07:46 +0000

    removed verbose commands from zsh

--- infrastructure/hack/zsh.sh
+++ infrastructure/hack/zsh.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
 install_zsh() {
-    # Install Zsh
     sudo apt update
     sudo apt install -y zsh
-
-    # Install Oh My Zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    # Install Autosuggestions plugin for Oh My Zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-    # Enable Autosuggestions plugin
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
-
-    # Set Zsh as the default shell
     chsh -s "$(which zsh)"
-
-    # Restart the shell
     exec zsh
 }
 
-# Call the function
 install_zsh
