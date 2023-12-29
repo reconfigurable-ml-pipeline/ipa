09c263a58822c30b3a396713fa936f82acb7821d
Chameleon Cloud User <cc@saeid-ipa-public.novalocal>
2023-12-29 18:08:16 +0000

    added revised version experiments

--- infrastructure/hack/jupyters.sh
+++ infrastructure/hack/jupyters.sh
@@ -14,6 +14,14 @@ execute_notebooks() {
     ~/ipa/experiments/runner/notebooks/paper-fig14-objecitve-preference.ipynb
     ~/ipa/experiments/runner/notebooks/paper-fig15-comparison-cdf.ipynb
     ~/ipa/experiments/runner/notebooks/paper-fig16-predictor-abelation.ipynb
+
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig8-e2e-video.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig9-e2e-audio-qa.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig10-e2e-audio-sent.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig11-e2e-sum-qa.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig12-e2e-nlp.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig14-objecitve-preference.ipynb
+    ~/ipa/experiments/runner/notebooks/paper-revision-fig15-comparison-cdf.ipynb
   )
 
   # Loop through the notebooks and execute them
