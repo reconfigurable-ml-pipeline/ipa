66481cfa74553302ce7282b756342f0396cae1c6
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2024-01-03 20:14:38 +0000

    fixed copying from google cloud storage

--- infrastructure/hack/download_data.sh
+++ infrastructure/hack/download_data.sh
@@ -6,8 +6,14 @@ function download_data(){
     rm ~/ipa/data/results.zip
 
     # download ml models
-    gsutil cp -rn 'gs://ipa-models/myshareddir/torchhub' /mnt/myshareddir
-    gsutil cp -rn 'gs://ipa-models/myshareddir/huggingface' /mnt/myshareddir
+    mc mb minio/huggingface
+    mc mb minio/torchhub
+    mkdir ~/temp-model-dir
+    gsutil cp -rn 'gs://ipa-models/myshareddir/torchhub' ~/temp-model-dir
+    gsutil cp -rn 'gs://ipa-models/myshareddir/huggingface' ~/temp-model-dir
+    mc cp -r ~/temp-model-dir/huggingface/* minio/huggingface
+    mc cp -r ~/temp-model-dir/torchhub/* minio/torchhub
+    rm -r ~/temp-model-dir
 
     # download lstm trained model
     gsutil cp -r gs://ipa-models/lstm ~/ipa/data
